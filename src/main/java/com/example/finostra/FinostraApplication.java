afee24d59e547adbe79118e44ea545cf24bb63f9
Nazar <nazikkryvycko@gmail.com>
2024-12-07 12:53:24 +0200

    user reg system

---
+++ src/main/java/com/example/finostra/FinostraApplication.java
@@ -0,0 +1,13 @@
+package com.example.finostra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinostraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinostraApplication.class, args);
+    }
+
+}
